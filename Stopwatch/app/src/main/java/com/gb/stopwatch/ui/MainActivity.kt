0580755777dc6380c8f38f762825c31f2cf93c5a
Rinat <mr.rino@gmail.com>
2022-04-02 20:20:40 +0300

    Added view binding to the project

--- Stopwatch/app/src/main/java/com/gb/stopwatch/ui/MainActivity.kt
+++ Stopwatch/app/src/main/java/com/gb/stopwatch/ui/MainActivity.kt
@@ -1,15 +1,13 @@
 package com.gb.stopwatch.ui
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.gb.stopwatch.R
 import com.gb.stopwatch.core.formatter.TimestampMillisecondsFormatterImpl
+import com.gb.stopwatch.core.provider.TimestampProviderImpl
+import com.gb.stopwatch.databinding.ActivityMainBinding
 import com.gb.stopwatch.usecase.ElapsedTimeCalculatorImpl
 import com.gb.stopwatch.usecase.StopwatchStateCalculatorImpl
 import com.gb.stopwatch.usecase.StopwatchStateHolderImpl
-import com.gb.stopwatch.core.provider.TimestampProviderImpl
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
@@ -17,6 +15,8 @@ import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     private val timestampProvider = TimestampProviderImpl()
 
@@ -37,25 +37,29 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val textView = findViewById<TextView>(R.id.text_time)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
         CoroutineScope(
             Dispatchers.Main
                     + SupervisorJob()
         ).launch {
             stopwatchListOrchestrator.ticker.collect {
-                textView.text = it
+                binding.textTime.text = it
             }
         }
 
-        findViewById<Button>(R.id.button_start).setOnClickListener {
+        binding.buttonStart.setOnClickListener {
             stopwatchListOrchestrator.start()
         }
-        findViewById<Button>(R.id.button_pause).setOnClickListener {
+
+        binding.buttonPause.setOnClickListener {
             stopwatchListOrchestrator.pause()
         }
-        findViewById<Button>(R.id.button_stop).setOnClickListener {
+
+        binding.buttonStop.setOnClickListener {
             stopwatchListOrchestrator.stop()
         }
 
